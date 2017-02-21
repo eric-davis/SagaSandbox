f75a9f0e71c9aa036a911ba635e0cb9b9bdc20fa
Eric Davis <eric.davis74@gmail.com>
2017-02-20 20:20:09 -0500

    Add project files.

---
+++ src/Saga/EngagementDisengagedSaga.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VMS
+{
+    public sealed class EngagementDisengagedSaga
+    {
+        public Guid Id { get; }
+
+        public int UserId { get; }
+
+        public DateTime CreatedDateUtc { get; }
+
+        public DateTime? UpdatedDateUtc { get; }
+
+        public DateTime? CompletedDate { get; }
+
+        public EngagementDisengagementSagaStatus Status {get;}
+
+        public Dictionary<int, DisengagementStatus> EngagementStatuses { get; }
+
+        public void Apply<TEvent>(TEvent e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum EngagementDisengagementSagaStatus
+    {
+        Pending,
+        InProcess,
+        Failed,
+        Complete,
+        CompleteWithErrors
+    }
+
+    public enum DisengagementStatus
+    {
+        InProcess,
+        Failed,
+        Complete
+    }
+}
